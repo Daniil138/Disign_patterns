1915cb8a200b323347c50185c5493570ab006fc7
danil138g <futurino120.bom@gmail.com>
2024-02-25 23:04:10 +0800

    programm

--- Programm/Src/settings.py
+++ Programm/Src/settings.py
@@ -1,103 +1,51 @@
+from Src.exceptions import exception_proxy
 
-
-class settings:
-    __first_name = ""
-    __INN = ""
-    __coresp_schet = None
-    __BIK = None
-    __name_service = ""
-    __type_prop = ""
-
-
-    @property
-    def first_name(self):
-        return self.__first_name
+#
+# Класс для описания настроек
+#
+class settings():
+    _inn = 0
+    _short_name = ""
+    _first_start = True
+    
     
     @property
-    def INN(self):
-        return self.__INN
+    def inn(self):
+        """
+            ИНН
+        Returns:
+            int: 
+        """
+        return self._inn
     
-    @property
-    def coresp_schet(self):
-        return self.__coresp_schet
+    @inn.setter
+    def inn(self, value: int):
+        exception_proxy.validate(value, int)
+        self._inn = value
+         
+    @property     
+    def short_name(self):
+        """
+            Короткое наименование организации
+        Returns:
+            str:
+        """
+        return self._short_name
     
-    @property
-    def BIK(self):
-        return self.__BIK
-    
-    @property
-    def name_service(self):
-        return self.__name_service
-    
-        
-    @property
-    def type_prop(self):
-        return self.__type_prop
-    
-    
-    
-    
-    @first_name.setter
-    def first_name(self, value: str):
-        """
-        Получение значения first_name и проверка на тип 
-        """
-        if not isinstance(value, str):
-            raise Exception("Некорректный аргумент!")
-        
-        self.__first_name = value.strip()
-
-    @INN.setter
-    def INN(self, value: str):
-        """
-        Получение значения INN и проверка на тип и длинну 
-        """
-        if (not isinstance(value, int)) and len(str(value))==11:
-            raise Exception("Некорректный аргумент!")
-        
-        self.__INN = value
-    
-    @coresp_schet.setter
-    def coresp_schet(self, value: str):
-        """
-        Получение значение кореспонденского счета  и проверка на тип и длинну 
-        """
-        if (not isinstance(value, int)) and len(str(value))==11:
-            raise Exception("Некорректный аргумент!")
-        
-        self.__coresp_schet = value
-
-
-    @BIK.setter
-    def BIK(self, value: str):
-        """
-        Получение значение BIK и проверка на тип и длинну 
-        """
-        if (not isinstance(value, int)) and len(str(value))==11:
-            raise Exception("Некорректный аргумент!")
-        
-        self.__BIK = value
-
-    @name_service.setter
-    def name_service(self, value: str):
-        """
-        Получение вида услуги  и проверка на тип 
-        """
-        if (not isinstance(value, str)) :
-            raise Exception("Некорректный аргумент!")
-        
-        self.__name_service = value.strip()
-
-    @type_prop.setter
-    def typ_prop(self, value: str):
-        """
-        Получение вида собственности  и проверка на тип и длинну 
-        """
-        if (not isinstance(value, str)) and len(value)<=5 :
-            raise Exception("Некорректный аргумент!")
-        
-        self.__type_prop = value.strip()
-
-    
+    @short_name.setter
+    def short_name(self, value:str):
+        exception_proxy.validate(value, str)
+        self._short_name = value
         
         
+    @property    
+    def is_first_start(self):
+        """
+           Флаг Первый старт
+        """
+        return self._first_start    
+            
+    @is_first_start.setter        
+    def is_first_start(self, value: bool):
+        self._first_start = value
+    
